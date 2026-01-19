eb1275a66150e479378ffe10602a32fde1b07f44
Madyan.21 <madyan.m005@gmail.com>
2026-01-19 14:28:26 +0300

    update

--- lib/Lecture17/task_stack.dart
+++ lib/Lecture17/task_stack.dart
@@ -7,112 +7,142 @@ class TaskStack extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sign in", style: TextStyle(color: Colors.white)),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {},
+        ),
+
+        title: const Text("Sign in", style: TextStyle(color: Colors.white)),
         centerTitle: true,
-        backgroundColor: Colors.purple,
+        backgroundColor: const Color.fromARGB(255, 130, 14, 150),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],
       ),
+      body: Column(
+        children: [
+          const SizedBox(height: 20),
 
-      body: SingleChildScrollView(
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: Center(
-                child: Column(
-                  children: [
-                    Container(
-                      width: 70,
-                      height: 70,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.black,
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            ("https://media.cnn.com/api/v1/images/stellar/prod/200825155834-lionel-messi-0307-file.jpg?q=w_3000,h_2000,x_0,y_0,c_fill"),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 15),
-                    Text(
-                      "Lionel Messi",
-                      style: TextStyle(fontSize: 15, fontWeight: .bold),
-                    ),
-
-                    const SizedBox(height: 20),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: List.generate(
-                        4,
-                        (index) => Container(
-                          margin: const EdgeInsets.all(6),
-                          width: 18,
-                          height: 18,
-                          decoration: BoxDecoration(
-                            color: Colors.grey.shade400,
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 20), 
-
-                    SizedBox(
-                      height: 400,
-                      child: Card(
-                        elevation: 25,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(12),
-                          child: GridView.count(
-                            shrinkWrap: true,
-                            crossAxisCount: 3,
-                            // mainAxisSpacing: 5,
-                            // crossAxisSpacing: 5,
-                            physics: const NeverScrollableScrollPhysics(),
-                            children:
-                                [
-                                  '1',
-                                  '2',
-                                  '3',
-                                  '4',
-                                  '5',
-                                  '6',
-                                  '7',
-                                  '8',
-                                  '9',
-                                  '',
-                                  '0',
-                                  '⌫',
-                                ].map((e) {
-                                  return Center(
-                                    child: Text(
-                                      e,
-                                      style: const TextStyle(
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                  );
-                                }).toList(),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+          Container(
+            width: 70,
+            height: 70,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                  "https://media.cnn.com/api/v1/images/stellar/prod/200825155834-lionel-messi-0307-file.jpg?q=w_3000,h_2000,x_0,y_0,c_fill",
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+
+          const SizedBox(height: 15),
+
+          const Text(
+            "Lionel Messi",
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          ),
+
+          const SizedBox(height: 20),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              customContainer(),
+              customContainer(),
+              customContainer(),
+              customContainer(),
+            ],
+          ),
+
+          const SizedBox(height: 45),
+
+          SizedBox(
+            height: 370,
+            width: double.infinity,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(height: 200, color: Colors.purple),
+                ),
+
+                Positioned(
+                  bottom: 40,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: Offset(0, 10),
+                        ),
+                      ],
+                      color: Colors.white,
+                    ),
+                    width: 250,
+                    height: 320,
+
+                    child: Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: GridView.count(
+                        shrinkWrap: true,
+                        crossAxisCount: 3,
+
+                        children:
+                            [
+                              '1',
+                              '2',
+                              '3',
+                              '4',
+                              '5',
+                              '6',
+                              '7',
+                              '8',
+                              '9',
+                              '',
+                              '0',
+                              '⌫',
+                            ].map((e) {
+                              return Center(
+                                child: Text(
+                                  e,
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              );
+                            }).toList(),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
- 
+  static Widget customContainer() {
+    return Container(
+      width: 21,
+      height: 21,
+      margin: const EdgeInsets.all(6),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade400,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
 }
