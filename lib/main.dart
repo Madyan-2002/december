b159486571763f97d03d3b2babfb71b1bf831960
Madyan.21 <madyan.m005@gmail.com>
2026-02-09 15:42:42 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,4 @@
 import 'package:december/Gofood/screens/splash_screen.dart';
-import 'package:december/Lecture01/home_screen.dart';
-import 'package:december/Lecture02/rich_text.dart';
-import 'package:december/Lecture03/text_review.dart';
-import 'package:december/Lecture04/container_lec.dart';
-import 'package:december/Lecture05/icon_widget.dart';
-import 'package:december/Lecture06/image_widget.dart';
-import 'package:december/Lecture07/image_in_container.dart';
-import 'package:december/Lecture10/buttons.dart';
-import 'package:december/Lecture08/screen_call1.dart';
-import 'package:december/Lecture08/task.dart';
-import 'package:december/Lecture10/task_buttons.dart';
-import 'package:december/Lecture11/colors_class.dart';
-import 'package:december/Lecture11/task22.dart';
-import 'package:december/Lecture11/training.dart';
-import 'package:december/Lecture15/task_index.dart';
-import 'package:december/Lecture16/task_grid.dart';
-import 'package:december/Lecture17/task_stack.dart';
-import 'package:december/Lecture18/list_view_class.dart';
-import 'package:december/Lecture19/views/grid_view_advance.dart';
-import 'package:december/Lecture19/views/list_view_advance.dart';
-import 'package:december/Lecture19/views/stack25.dart';
-import 'package:december/Lecture20/nav_screen_one.dart';
-import 'package:december/Lecture20/nav_screen_three.dart';
-import 'package:december/Lecture20/nav_screen_two.dart';
-import 'package:december/Lecture21/form_class.dart';
-import 'package:december/Lecture21/text_field_class.dart';
-import 'package:december/Lecture23/date_time.dart';
-import 'package:december/Lecture23/img_custom_slider.dart';
-import 'package:december/Lecture23/img_slider.dart';
-import 'package:december/Lecture23/nav.dart';
-import 'package:december/Lecture23/time.dart';
-import 'package:december/Lecture23/timeAnddate.dart';
-import 'package:december/responsive/layout_responsive.dart';
-import 'package:december/responsive/mobile.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -99,7 +65,6 @@ class MyApp extends StatelessWidget {
       //home: NavScreenOne(),
       //home: FormClass(),
       //home:TaskStack()
-
       //home:Nav()
       //home:ImgSlider()
       //home:ImgCustomSlider()
